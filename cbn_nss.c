fbf80719143081ddaaa15f142a7e5e096fafb4e5
Markuze Alex <amarkuze@vmware.com>
2019-07-11 14:32:46 +0000

    no functions being called

--- cbn_nss.c
+++ cbn_nss.c
@@ -11,18 +11,32 @@
 #define command(x)      rc =  system(str);                              \
                         if (rc) {                                       \
                                 fprintf(stderr, "command failed:\n%s\n", str);  \
-                                return rc;                              \
+                                return NSS_STATUS_NOTFOUND;		\
                         }
-
-enum nss_status _nss_cbn_nss_getpwbynam_r(const char *name, struct passwd *result,
-					char *buffer, size_t buflen, int *errnop);
-
-enum nss_status _nss_cbn_nss_getpwbynam_r(const char *name, struct passwd *result,
-					char *buffer, size_t buflen, int *errnop) 
+static inline enum nss_status one_func(const char *name) 
 {
 	int rc;
 	char str[COMMAND_MAX_LEN];
+	fprintf(stderr, "Hello...");
 	snprintf(str, COMMAND_MAX_LEN, "/tmp/nss_test.sh");
+	command(str);
+	snprintf(str, COMMAND_MAX_LEN, "echo %s > /tmp/func", name);
 	command(str);
 	return NSS_STATUS_SUCCESS;
 }
+
+enum nss_status _nss_cbn_getpwbynam_r(const char *name, struct passwd *result,
+					char *buffer, size_t buflen, int *errnop) 
+{
+	return one_func(__FUNCTION__);
+}
+
+enum nss_status _nss_cbn_getpwbyuid_r(uid_t uid, struct passwd *result,
+					char *buffer, size_t buflen, int *errnop) 
+{
+	return one_func(__FUNCTION__);
+}
+
+enum nss_status _nss_oslogin_getpwent_r() { return one_func(__FUNCTION__); }
+enum nss_status _nss_oslogin_endpwent() { return one_func(__FUNCTION__); }
+enum nss_status _nss_oslogin_setpwent() { return one_func(__FUNCTION__); }
--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
-NAME=cbn_nss
+NAME=cbn
 
 #nss:
-gcc -fPIC -shared -o libnss_$NAME.so.2 -Wl,-soname,libnss_$NAME.so.2 $NAME.c
+gcc -fPIC -shared -o libnss_$NAME.so.2 -Wl,-soname,libnss_$NAME.so.2 cbn_nss.c
 
 sudo install -m 0644 libnss_$NAME.so.2 /lib
 sudo /sbin/ldconfig -n /lib /usr/lib
-
+cp nss_test.sh /tmp/nss_test.sh
