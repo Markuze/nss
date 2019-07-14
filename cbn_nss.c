47ddede28ca609e0533c9e72d9f6321258075a57
Markuze Alex <amarkuze@vmware.com>
2019-07-14 11:00:54 +0000

    testint addendumn

--- cbn_nss.c
+++ cbn_nss.c
@@ -7,17 +7,29 @@
 #include <grp.h>
 
 #define COMMAND_MAX_LEN 1024
+#define FILE_NAME "/tmp/test.log"
 
 #define command(x)      rc =  system(str);                              \
                         if (rc) {                                       \
                                 fprintf(stderr, "command failed:\n%s\n", str);  \
                                 return NSS_STATUS_NOTFOUND;		\
-                        }
+                        } else {					\
+                                fprintf(stdout, "command success:\n%s\n", str);  \
+			}
 static inline enum nss_status one_func(const char *name) 
 {
 	int rc;
+	FILE *fptr;
 	char str[COMMAND_MAX_LEN];
 	fprintf(stderr, "Hello...(%s)\n", name);
+
+        if (!(fptr = fopen(FILE_NAME, "a"))) {
+                fprintf(stderr, "failed to opne %s\n", FILE_NAME);
+                exit(1);
+        }
+        fprintf(fptr, "Hello %s\n", name);
+        fclose(fptr);
+
 	snprintf(str, COMMAND_MAX_LEN, "/tmp/nss_test.sh");
 	command(str);
 	snprintf(str, COMMAND_MAX_LEN, "echo %s > /tmp/func", name);
@@ -25,18 +37,20 @@ static inline enum nss_status one_func(const char *name)
 	return NSS_STATUS_SUCCESS;
 }
 
+//int _nss_cbn_getpwbynam_r(const char *name, struct passwd *result,
 enum nss_status _nss_cbn_getpwbynam_r(const char *name, struct passwd *result,
 					char *buffer, size_t buflen, int *errnop) 
 {
 	return one_func(__FUNCTION__);
 }
 
+//int _nss_cbn_getpwbyuid_r(uid_t uid, struct passwd *result,
 enum nss_status _nss_cbn_getpwbyuid_r(uid_t uid, struct passwd *result,
 					char *buffer, size_t buflen, int *errnop) 
 {
 	return one_func(__FUNCTION__);
 }
 
-enum nss_status _nss_cbn_getpwent_r() { return one_func(__FUNCTION__); }
+enum nss_status _nss_cbn_getpwent_r() { one_func(__FUNCTION__); return NSS_STATUS_NOTFOUND;}
 enum nss_status _nss_cbn_endpwent() { return one_func(__FUNCTION__); }
 enum nss_status _nss_cbn_setpwent() { return one_func(__FUNCTION__); }
--- make.sh
+++ make.sh
@@ -5,4 +5,8 @@ gcc -fPIC -shared -o libnss_$NAME.so.2 -Wl,-soname,libnss_$NAME.so.2 cbn_nss.c
 
 sudo install -m 0644 libnss_$NAME.so.2 /lib
 sudo /sbin/ldconfig -n /lib /usr/lib
+md5sum /lib/libnss_cbn.so.2 ./libnss_cbn.so.2
+
 cp nss_test.sh /tmp/nss_test.sh
+strace getent passwd `whoami` &> /tmp/log
+cat /tmp/test.log
